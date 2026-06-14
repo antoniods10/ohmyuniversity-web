5ca88311e2a95a407278b8bb24a51d8b6cd704c3
Luca D'Aurizio <daurizio.lc@gmail.com>
2026-06-14 19:25:48 +0200

    feat: add temp settings dashboard page

---
+++ src/app/features/dashboard/presentation/pages/settings/settings.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-settings-page',
+  standalone: true,
+  templateUrl: './settings.page.html',
+})
+export class SettingsPage {}
