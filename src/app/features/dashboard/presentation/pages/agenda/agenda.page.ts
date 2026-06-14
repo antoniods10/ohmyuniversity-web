98cda2fd3d9b251ae760da694075e836938046bd
Luca D'Aurizio <daurizio.lc@gmail.com>
2026-06-14 19:13:27 +0200

    feat: add temp agenda dashboard page

---
+++ src/app/features/dashboard/presentation/pages/agenda/agenda.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-agenda-page',
+  standalone: true,
+  templateUrl: './agenda.page.html',
+})
+export class AgendaPage {}
